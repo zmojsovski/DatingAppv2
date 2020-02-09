60ea15fc7acedb9f05023d50e22ccc901087b519
Foronisus <zankomojsoski@yahoo.com>
2020-02-09 15:24:50 +0100

    Initial Commit

---
+++ DatingApp2/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using DatingApp2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp2.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)  { }
+
+        public DbSet<Value> Values {get;set;}
+    }
+}
