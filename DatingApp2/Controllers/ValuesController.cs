14ac1cf82b583388f5ba10fdb3f96cf1a08fd12d
Foronisus <zankomojsoski@yahoo.com>
2020-02-16 23:57:55 +0100

    Angular login/register

--- DatingApp2/Controllers/ValuesController.cs
+++ DatingApp2/Controllers/ValuesController.cs
@@ -21,7 +21,7 @@ namespace DatingApp2.Controllers
 
         }
         // GET api/values
-        
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
